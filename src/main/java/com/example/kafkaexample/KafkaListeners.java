aa99aeb83b563c5433072ff9f84d5a4fe086f619
unknown <uzair6930@gmail.com>
2023-08-27 20:41:01 +0500

    Final Commit

---
+++ src/main/java/com/example/kafkaexample/KafkaListeners.java
@@ -0,0 +1,13 @@
+package com.example.kafkaexample;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(topics = "amigoscode", groupId = "groupid")
+    void listener(String data){
+        System.out.println("Listener received: " + data + ":)");
+    }
+}
